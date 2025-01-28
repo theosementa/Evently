49fc75828b9e5db6ed122d0c88db2a1e55992dd8
Theo Sementa <theosementa@gmail.com>
2025-01-28 20:47:56 +0100

    Initial Commit

---
+++ Evently/EventlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventlyApp.swift
+//  Evently
+//
+//  Created by Theo Sementa on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct EventlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
