47a522275ea7e312f9b6e6803984d52b318250f4
Theo Sementa <theosementa@gmail.com>
2025-02-02 20:48:58 +0100

    feat: add Category model

---
+++ Evently/Core/Network/APIRequester/CategoryAPIRequester.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryAPIRequester.swift
+//  Evently
+//
+//  Created by Theo Sementa on 02/02/2025.
+//
+
+import Foundation
+
+//enum CategoryAPIRequester: APIRequestBuilder {
+//    case fetchAll
+//    case 
+//}
---
+++ Evently/Domain/Models/API/CategoryModel.swift
@@ -0,0 +1,43 @@
+//
+//  CategoryModel.swift
+//  Evently
+//
+//  Created by Theo Sementa on 02/02/2025.
+//
+
+import Foundation
+import SwiftUICore
+
+struct CategoryModel: Codable {
+    var id: Int?
+    var rawName: String?
+    var rawIcon: String?
+    var rawColor: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case rawName = "name"
+        case rawIcon = "icon"
+        case rawColor = "color"
+    }
+}
+
+extension CategoryModel {
+    
+    var name: String {
+        return rawName?.localized ?? ""
+    }
+    
+    var icon: String {
+        return rawIcon ?? "" // TODO: Prendre un exclamation mark
+    }
+    
+    var color: String {
+        return rawColor ?? ""
+    }
+    
+    var gradient: LinearGradient {
+        return LinearGradient(colorHex: color)
+    }
+    
+}
