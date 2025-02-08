f92d0756aa4418f3da8278bf37dbcafce1ef927e
Theo Sementa <theosementa@gmail.com>
2025-02-08 08:23:22 +0100

    feat: wip profile

---
+++ Evently/Features/Profile/Components/ProfileRow.swift
@@ -0,0 +1,48 @@
+//
+//  ProfileRow.swift
+//  Evently
+//
+//  Created by Theo Sementa on 08/02/2025.
+//
+
+import SwiftUI
+
+struct ProfileRow: View {
+
+    // builder
+    var icon: ImageResource
+    var title: String
+    var isPushable: Bool
+
+    // MARK: -
+    var body: some View {
+        HStack {
+            Image(icon)
+                .resizable()
+                .frame(width: 20, height: 20)
+
+            Text(title)
+                .font(.Content.mediumSemiBold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            Image(isPushable ? .chevronRight : .arrowDiagonal)
+                .resizable()
+                .frame(width: 20, height: 20)
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 14)
+        .background {
+            RoundedRectangle(cornerRadius: 16, style: .continuous)
+                .fill(Color.black100)
+                .stroke(Color.black200, lineWidth: 1)
+        }
+    } // body
+} // struct
+
+// MARK: - Preview
+#Preview {
+    ProfileRow(icon: .person, title: "My friends", isPushable: true)
+        .preferredColorScheme(.dark)
+        .padding()
+        .background(Color.black0)
+}
--- Evently/Features/Profile/Views/ProfileView.swift
+++ Evently/Features/Profile/Views/ProfileView.swift
@@ -9,20 +9,68 @@ import SwiftUI
 
 struct ProfileView: View {
 
+    @Environment(\.dismiss) private var dismiss
+
     @Environment(UserStore.self) private var userStore
-    @Environment(FriendStore.self) private var friendStore
 
     // MARK: -
     var body: some View {
         VStack(spacing: 16) {
-            if let currentUser = userStore.currentUser {
-                Text("Hello, \(currentUser.fullName)!")
-                Text(currentUser.username ?? "")
+            HStack {
+                TinyActionButton(icon: .chevronBack) { dismiss() }
+                Spacer()
             }
-            ForEach(friendStore.friends) { friend in
-                Text(friend.fullName)
-            }
+
+            ScrollView {
+                VStack(spacing: 48) {
+                    if let currentUser = userStore.currentUser {
+                        VStack(spacing: 8) {
+                            Text(currentUser.fullName)
+                                .font(.Headline.head4)
+                            Button(action: {
+                                UIPasteboard.general.string = currentUser.username ?? ""
+                                BannerManager.shared.banner = Banner.usernameCopied
+                            }, label: {
+                                HStack(spacing: 8) {
+                                    Image(.copy)
+                                        .resizable()
+                                        .frame(width: 20, height: 20)
+                                    Text(currentUser.username ?? "")
+                                        .font(.Content.largeSemiBold)
+                                }
+                            })
+                        }
+                        .foregroundStyle(Color.white0)
+                    }
+
+                    VStack(spacing: 16) {
+                        ProfileRow(
+                            icon: .person,
+                            title: "Mes amis",
+                            isPushable: true
+                        ) // TODO: TBL
+                        ProfileRow(
+                            icon: .hammer,
+                            title: "Mes préférences",
+                            isPushable: true
+                        ) // TODO: TBL
+                        ProfileRow(
+                            icon: .gear,
+                            title: "Gérer mon compte",
+                            isPushable: true
+                        ) // TODO: TBL
+                    }
+                    .padding(.horizontal, 1)
+
+                }
+            } // ScrollView
+            .scrollIndicators(.hidden)
         }
+        .padding(24)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black0)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarTitleDisplayMode(.inline)
     } // body
 } // struct
 
